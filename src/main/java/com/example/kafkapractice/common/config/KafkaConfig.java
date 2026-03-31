d4370da4c361fa98a73357bdeafca11a07bc0592
Soomin <soomin02094@gmail.com>
2026-03-31 15:09:12 +0900

    feat: Acknowledgment 사용해 수동 Commit 구현

--- src/main/java/com/example/kafkapractice/common/config/KafkaConfig.java
+++ src/main/java/com/example/kafkapractice/common/config/KafkaConfig.java
@@ -20,6 +20,7 @@ import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.support.serializer.JacksonJsonDeserializer;
 import org.springframework.kafka.support.serializer.JacksonJsonSerializer;
 
@@ -137,6 +138,34 @@ public class KafkaConfig {
         ConcurrentKafkaListenerContainerFactory<String, SimpleEvent> factory = new ConcurrentKafkaListenerContainerFactory<>();
 
         factory.setConsumerFactory(eventConsumerFactory());
+        return factory;
+    }
+
+
+    // 수동 Commit
+    @Bean
+    public ConsumerFactory<String, String> manualConsumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "manual-ack-group");
+
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> manualAckKafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+
+        factory.setConsumerFactory(manualConsumerFactory());
+
+        // AckMode를 MANUAL로 설정
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
         return factory;
     }
 }
---
+++ src/main/java/com/example/kafkapractice/domain/simple/listener/ManualAckConsumerListener.java
@@ -0,0 +1,29 @@
+package com.example.kafkapractice.domain.simple.listener;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class ManualAckConsumerListener {
+
+    @KafkaListener(
+            topics = "simple-messages",
+            groupId = "manual-ack-group",
+            containerFactory = "manualAckKafkaListenerContainerFactory"
+    )
+    public void consume(ConsumerRecord<String, String> record, Acknowledgment ack) {
+        String message = record.value();
+        log.info("[Manual ACK 모드] 받은 메시지 : {}", message);
+
+        if (message.equals("error")) {
+            throw new RuntimeException("kafka 에러 발생");
+        }
+
+        // 수동 Commit
+        ack.acknowledge();
+    }
+}
